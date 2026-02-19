6a10bbf60793593f5cbe4d75019f310ccb702220
Akhilesh Mandawgane <akhileshmandawganeofficial@gmail.com>
2026-02-19 14:17:32 +0530

    login and register controller added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,117 @@
+const userModel = require("../models/user.model");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+
+
+
+// register controllers
+async function registerController (req, res) {
+  const { name, email, username, password, bio, profileImage } = req.body;
+
+  // we use $or for if user exits with username or email it must return in the callback
+  const isUserExits = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (isUserExits) {
+    return res.status(409).json({
+      message:
+        "User already exits " +
+        (isUserExits.email == email
+          ? "Email already exits"
+          : "Username already exits"),
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  const user = await userModel.create({
+    username,
+    email,
+    profileImage,
+    password: hash,
+    bio,
+    name,
+  });
+
+  //   it must contain user data and it must be unique
+  //   token is created by server using users data to authenticate
+  const token = jwt.sign(
+    {
+      id: user._id,
+      name: user.name,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+
+  console.log(name);
+
+  res.status(201).json({
+    message: "User Registered Sucessfully",
+    user: {
+      email: user.email,
+      username: user.username,
+      name: user.name,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    },
+  });
+}
+
+
+// login controller 
+async  function loginController (req,res) {
+    async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // if user can login with username and pass or email with pass
+  const user = await userModel.findOne({
+    $or: [
+      {
+        email: email,
+      },
+      {
+        username: username,
+      },
+    ],
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  const isPasswordValid = hash == user.password;
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "password invalid",
+    });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("token", token)
+
+  res.status(200).json({
+    message: "User login Sucessfully",
+    user: {
+        username: user.username,
+        email: user.email,
+        bio: user.bio,
+        profileImage: user.profileImage
+    }
+  })
+
+}}
+
+
+module.exports = {registerController , loginController}
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,114 +1,16 @@
 const express = require("express");
-const userModel = require("../models/user.model");
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
+const authController = require('../controllers/auth.controller')
 
 const authRouter = express.Router();
 
 // register API
-authRouter.post("/register", async (req, res) => {
-  const { name, email, username, password, bio, profileImage } = req.body;
+authRouter.post("/register", authController.registerController);
 
-  // we use $or for if user exits with username or email it must return in the callback
-  const isUserExits = await userModel.findOne({
-    $or: [{ username }, { email }],
-  });
-
-  if (isUserExits) {
-    return res.status(409).json({
-      message:
-        "User already exits " +
-        (isUserExits.email == email
-          ? "Email already exits"
-          : "Username already exits"),
-    });
-  }
-
-  const hash = crypto.createHash("sha256").update(password).digest("hex");
-
-  const user = await userModel.create({
-    username,
-    email,
-    profileImage,
-    password: hash,
-    bio,
-    name,
-  });
-
-  //   it must contain user data and it must be unique
-  //   token is created by server using users data to authenticate
-  const token = jwt.sign(
-    {
-      id: user._id,
-      name: user.name,
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" },
-  );
-
-  res.cookie("token", token);
-
-  console.log(name);
-
-  res.status(201).json({
-    message: "User Registered Sucessfully",
-    user: {
-      email: user.email,
-      username: user.username,
-      name: user.name,
-      bio: user.bio,
-      profileImage: user.profileImage,
-    },
-  });
-});
 
 // login
-authRouter.post("/login", async (req, res) => {
-  const { username, email, password } = req.body;
+authRouter.post("/login", authController.loginController);
 
-  // if user can login with username and pass or email with pass
-  const user = await userModel.findOne({
-    $or: [
-      {
-        email: email,
-      },
-      {
-        username: username,
-      },
-    ],
-  });
 
-  if (!user) {
-    return res.status(404).json({
-      message: "User not found",
-    });
-  }
 
-  const hash = crypto.createHash("sha256").update(password).digest("hex");
-
-  const isPasswordValid = hash == user.password;
-
-  if (!isPasswordValid) {
-    return res.status(401).json({
-      message: "password invalid",
-    });
-  }
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
-
-  res.cookie("token", token)
-
-  res.status(200).json({
-    message: "User login Sucessfully",
-    user: {
-        username: user.username,
-        email: user.email,
-        bio: user.bio,
-        profileImage: user.profileImage
-    }
-  })
-});
 
 module.exports = authRouter;
